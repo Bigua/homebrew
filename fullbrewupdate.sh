216425aa9a15c29896b5015a988a19b4727fe0e3
Ademar Peixoto <bigua.kun@gmail.com>
2020-12-10 23:33:23 -0300

    atualizando novos jeitos de invocar brew cask

--- fullbrewupdate.sh
+++ fullbrewupdate.sh
@@ -11,25 +11,24 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
-echo -e "$Green atualizando o hombebrew "
 brew update
-echo  -e "$Green instalando os pacotes"
+echo  -e "$Green atualizando os apps"
 brew upgrade
-echo  -e "$Cyan instalando os casks"
-for c in $(brew cask list); do
-        info=$(brew cask info $c)
-        current_ver=$(echo "$info" | sed '2,$d' | cut -d':' -f2| xargs)
+brew upgrade --cask
+echo  -e "$Cyan atualizando os casks manualmente"
+for c in $(brew list --cask); do
+        info=$(brew info --cask $c)
+        current_ver=$(echo "$info" | sed 's/(auto_updates)//g' | sed '2,$d' | cut -d':' -f2| xargs)
         installed_ver=$(echo "$info" |cut -d' ' -f1  | rev | cut -d'/' -f 1 | rev | sed '4,$d' | sed '1,2d')
         if [[ "$installed_ver" != *"$current_ver"* ]]; then
-                echo -e "$Yellow  $c installed is $installed_ver , current is $current_ver"
-                echo -e "$Cyan $c needs reinstall"
-                brew cask fetch  $c
-                brew cask uninstall --force $c
-                brew cask install $c
+                echo -e "$Purple  $c installed it's $installed_ver , current its $current_ver"
+                echo -e "$Yellow $c needs reinstall"
+                brew fetch --cask $c
+                brew uninstall --cask --force $c
+                brew install --cask $c
         fi
 done
 echo -e "$Cyan limpando"
 brew cleanup
-brew cask cleanup
-echo -e "$Yellow  verificar se nada quebrou... $Color_Off"
+echo -e "$Purple  verificar se nada quebrou... $Color_Off"
 brew doctor
